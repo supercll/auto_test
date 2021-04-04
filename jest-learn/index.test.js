319360cd6bfbbc96afba5aa0af9ff2f05365da33
lc <1033581609@qq.com>
2021-04-04 10:34:19 +0800

    add: Jest基本使用

---
+++ jest-learn/index.test.js
@@ -0,0 +1,13 @@
+const math = require("./index.js");
+const { add, minus } = math;
+
+test("测试加法", () => {
+  expect(add(3, 7)).toBe(10);
+});
+test("测试减法", () => {
+  expect(minus(3, 3)).toBe(0);
+});
+test("测试乘法", () => {
+  expect(minus(3, 3)).toBe(9);
+});
+// 运行：yarn test
